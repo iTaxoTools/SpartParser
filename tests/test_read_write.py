5d5a1d02ee40aa476baacc32d9a50b169462bf5b
StefanPatman <stefanpatman91@gmail.com>
2022-10-02 12:57:30 +0300

    Implement simple read-write test

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -2,9 +2,13 @@
 
 from pathlib import Path
 from dataclasses import dataclass
+from typing import Callable
 from enum import Enum, auto
 
 import pytest
+import re
+
+from itaxotools.spart_parser import Spart
 
 TEST_DATA_DIR = Path(__file__).parent / Path(__file__).stem
 
@@ -18,26 +22,23 @@ class SpartType(Enum):
 class ConversionTest:
     input: str
     output: str
-    type: SpartType
+    parser: Callable
+    writer: Callable
 
 
 test_data = [
-    ConversionTest('simple.spart', 'simple.xml', SpartType.XML),
-    ConversionTest('simple.xml', 'simple.spart', SpartType.Matricial),
+    ConversionTest('simple.spart', 'simple.xml', Spart.fromMatricial, Spart.toXML),
+    ConversionTest('simple.xml', 'simple.spart', Spart.fromXML, Spart.toMatricial),
     ]
 
 
 def assert_eq_files(file1: Path, file2: Path) -> None:
-    assert file1.read_text() == file2.read_text()
-
-
-def convert(input, output, type: SpartType):
-    if type is SpartType.Matricial:
-        with open(output, 'w') as file:
-            file.write('MATRICIAL\n')
-    elif type is SpartType.XML:
-        with open(output, 'w') as file:
-            file.write('XML\n')
+    regex = re.compile(r'[\s]')
+    text1 = file1.read_text()
+    text2 = file2.read_text()
+    text1 = regex.sub('', text1)
+    text2 = regex.sub('', text2)
+    assert text1 == text2
 
 
 @pytest.mark.parametrize("test", test_data)
@@ -45,5 +46,6 @@ def test_read_write(test: ConversionTest, tmp_path: Path) -> None:
     input_path = TEST_DATA_DIR / test.input
     output_path = TEST_DATA_DIR / test.output
     test_path = tmp_path / test.output
-    convert(input_path, test_path, test.type)
+    spart = test.parser(input_path)
+    test.writer(spart, test_path)
     assert_eq_files(test_path, output_path)
