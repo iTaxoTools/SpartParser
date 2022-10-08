944050a8861b874664032adc9109d33ca7bf03cd
StefanPatman <stefanpatman91@gmail.com>
2022-10-08 11:30:49 +0300

    Add scores test

--- tests/test_read.py
+++ tests/test_read.py
@@ -206,10 +206,103 @@ def spart_tagged(spart: Spart):
     assert spart.getSubsetIndividualData('spartition_3', '1', 'individual_3')['score'] == "3.3"
 
 
+def spart_scores(spart: Spart):
+    assert spart.project_name == 'scores_test'
+    assert spart.date == datetime.datetime(2022, 10, 2, 12, 0, 0)
+
+    # Validate individual list
+
+    individuals = spart.getIndividuals()
+    assert len(individuals) == 3
+    assert 'individual_1' in individuals
+    assert 'individual_2' in individuals
+    assert 'individual_3' in individuals
+    assert not spart.getIndividualData('individual_1')
+    assert not spart.getIndividualData('individual_2')
+    assert not spart.getIndividualData('individual_3')
+
+    # Validate spartition list
+
+    spartitions = spart.getSpartitions()
+    assert len(spartitions) == 3
+    assert 'spartition_1' in spartitions
+    assert 'spartition_2' in spartitions
+    assert 'spartition_3' in spartitions
+    assert spart.getSpartitionScore('spartition_1') == 0.1
+    assert spart.getSpartitionScore('spartition_2') == 0.2
+    assert spart.getSpartitionScore('spartition_3') == None
+
+    # Validate subset lists
+
+    subsets = spart.getSpartitionSubsets('spartition_1')
+    assert len(subsets) == 3
+    assert '1' in subsets
+    assert '2' in subsets
+    assert '3' in subsets
+    assert spart.getSubsetScore('spartition_1', '1') == 1.1
+    assert spart.getSubsetScore('spartition_1', '2') == 1.2
+    assert spart.getSubsetScore('spartition_1', '3') == None
+
+    subsets = spart.getSpartitionSubsets('spartition_2')
+    assert len(subsets) == 2
+    assert '1' in subsets
+    assert '2' in subsets
+    assert spart.getSubsetScore('spartition_2', '1') == 2.1
+    assert spart.getSubsetScore('spartition_2', '2') == 2.2
+
+    subsets = spart.getSpartitionSubsets('spartition_3')
+    assert len(subsets) == 1
+    assert '1' in subsets
+    assert spart.getSubsetScore('spartition_3', '1') == 3.1
+
+    # Validate 'spartition_1'
+
+    subset_individuals = spart.getSubsetIndividuals('spartition_1', '1')
+    assert len(subset_individuals) == 1
+    assert 'individual_1' in subset_individuals
+    assert spart.getSubsetIndividualScore('spartition_1', '1', 'individual_1') == 1.1
+
+    subset_individuals = spart.getSubsetIndividuals('spartition_1', '2')
+    assert len(subset_individuals) == 1
+    assert 'individual_2' in subset_individuals
+    assert spart.getSubsetIndividualScore('spartition_1', '2', 'individual_2') == 2.1
+
+    subset_individuals = spart.getSubsetIndividuals('spartition_1', '3')
+    assert len(subset_individuals) == 1
+    assert 'individual_3' in subset_individuals
+    assert spart.getSubsetIndividualScore('spartition_1', '3', 'individual_3') == 3.1
+
+    # Validate 'spartition_2'
+
+    subset_individuals = spart.getSubsetIndividuals('spartition_2', '1')
+    assert len(subset_individuals) == 2
+    assert 'individual_1' in subset_individuals
+    assert 'individual_2' in subset_individuals
+    assert spart.getSubsetIndividualScore('spartition_2', '1', 'individual_1') == 1.2
+    assert spart.getSubsetIndividualScore('spartition_2', '1', 'individual_2') == 2.2
+
+    subset_individuals = spart.getSubsetIndividuals('spartition_2', '2')
+    assert len(subset_individuals) == 1
+    assert 'individual_3' in subset_individuals
+    assert spart.getSubsetIndividualScore('spartition_2', '2', 'individual_3') == 3.2
+
+    # Validate 'spartition_3'
+
+    subset_individuals = spart.getSubsetIndividuals('spartition_3', '1')
+    assert len(subset_individuals) == 3
+    assert 'individual_1' in subset_individuals
+    assert 'individual_2' in subset_individuals
+    assert 'individual_3' in subset_individuals
+    assert spart.getSubsetIndividualScore('spartition_3', '1', 'individual_1') == None
+    assert spart.getSubsetIndividualScore('spartition_3', '1', 'individual_2') == 2.3
+    assert spart.getSubsetIndividualScore('spartition_3', '1', 'individual_3') == 3.3
+
+
 test_data = [
     ReadTest('simple.xml', Spart.fromXML, spart_simple),
     ReadTest('simple.spart', Spart.fromMatricial, spart_simple),
     ReadTest('tagged.xml', Spart.fromXML, spart_tagged),
+    ReadTest('scores.spart', Spart.fromMatricial, spart_scores),
     ]
 
 
